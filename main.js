4bdee5a88343dd0fbabe24ebde3bcefc789c81f6
SAANVI1976 <73863642+SAANVI1976@users.noreply.github.com>
2022-03-28 20:19:14 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+song1="";
+song2="";
+song1_status="";
+song2_status="";
+scoreRightWrist=0;
+scoreLeftWrist=0;
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+function preload(){
+    song1=loadSound("Music1.mp3");
+    song2=loadSound("Music2.mp3");
+}
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.position(350,150);
+    video=createCapture(VIDEO);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses);
+}
+function modelLoaded(){
+    console.log("poseNet is initialized");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        scoreRightWrist=results[0].pose.keypoints[10].score;
+        scoreLeftWrist=results[0].pose.keypoints[9].score;
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+      leftWristX=results[0].pose.leftWrist.x;
+      leftWristY=results[0].pose.leftWrist.y;
+    }
+}
+function draw(){
+    image(video,0,0,600,500);
+    song1_status=song1.isPlaying();
+    song2_status=song2.isPlaying();
+    fill("red");
+    stroke("red");
+    if(scoreRightWrist>0.2){
+        circle(rightWristX,rightWristY,20);
+        song2.stop();
+        if(song1_status==false){
+            song1.play();
+            document.getElementById("song").innerHTML="Playing-BTS-Save-Me-instrumentalfx.mp3";
+        }
+    }
+    if(scoreLeftWrist>0.2){
+        circle(leftWristX,leftWristY,20);
+        song1.stop();
+        if(song2_status==false){
+            song2.play();
+            document.getElementById("song").innerHTML="Playing-BTS_-_Idol_Ft_Nicki_Minaj__Luvmp.com.mp3";
+        }
+    }
+}
